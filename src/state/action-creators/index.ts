1764ec4e8c734090e25adaad8f9e94dd00e92679
g-ishi <al14006@shibaura-it.ac.jp>
2023-01-04 16:04:32 +0900

    Finish redux code

---
+++ src/state/action-creators/index.ts
@@ -0,0 +1,36 @@
+import axios from 'axios';
+import { Dispatch } from 'react';
+import { Action } from '../actions';
+import { ActionType } from '../action-types';
+
+const searchRepositories = (term: string) => {
+  return async (dispatch: Dispatch<Action>) => {
+    dispatch({
+      type: ActionType.SEARCH_REPOSITORIES,
+    });
+
+    try {
+      const { data } = await axios.get(
+        'https://registry.npmjs.org/-/v1/search',
+        {
+          params: {
+            text: term,
+          },
+        }
+      );
+      const names = data.objects.map((result: any) => {
+        return result.name;
+      });
+
+      dispatch({
+        type: ActionType.SEARCH_REPOSITORIES_SUCCESS,
+        payload: names,
+      });
+    } catch (error: any) {
+      dispatch({
+        type: ActionType.SEARCH_REPOSITORIES_ERROR,
+        payload: error.message,
+      });
+    }
+  };
+};
